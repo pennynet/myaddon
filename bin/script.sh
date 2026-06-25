fb68dcc106f2e95b2178cc7c8469601bd736e558
pennynet <deine-email@example.com>
2026-06-25 18:11:59 +0200

    Initial commit

---
+++ bin/script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# script.sh (v0.0.1)
+
+# in rasperrymatic-skript do:
+# dom.GetObject("CUxD.CUX2801001:1.CMD_EXEC").State("/usr/local/addons/myaddon/bin/script.sh");
+# ---------------------------------------------------------------------------------------------
+
+echo "Hello World!"
+
+# ---------------------------------------------------------------------------------------------
+
+# EOF
